48fdb69ed31e7d06dd887095d752b3c54b4d3a71
Dhanyavalli koppu <koppudhanyavalli123@gmail.com>
2026-02-27 14:42:50 +0530

    Delete java pratice directory

--- java pratice/Boxing.java
+++
@@ -1,9 +0,0 @@
-class Boxing 
-{
-    public static void main(String[] args)
-    {
-        int a = 50;
-        Integer obj = Integer.valueOf(a);
-        System.out.println(obj);
-    }
-}
--- java pratice/Unboxing.java
+++
@@ -1,9 +0,0 @@
-class Unboxing 
-{
-    public static void main(String args[])
-    {
-        Integer obj = 20;
-        int a = obj.intValue();
-        System.out.println(a);
-    }
-}
